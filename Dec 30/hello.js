9b4ee841b6c4ab97a540419a05b954f4f6a1523d
Vivek girhare <vivekgirhare123@gmail.com>
2026-01-01 05:18:11 +0530

    Add files via upload

---
+++ Dec 30/hello.js
@@ -0,0 +1,15 @@
+
+console.log("The sum of x and y is", sum);
+
+function A(){
+    var x = 10;
+    var y = 20;
+    var sum = x + y;
+    return sum;
+   
+}
+function B(){
+    x = 1;
+    console.log("the value of x in B is ")
+}
+    
